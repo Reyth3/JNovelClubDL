b009b3c1eb2f792be36c4dc3e5bfd43b2f4303de
Reyth3 <mrreynevan2@gmail.com>
2017-06-18 20:47:22 +0200

    Created Basic Setup for the Main Window

--- JNovelClubDL/MainWindow.xaml.cs
+++ JNovelClubDL/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using HtmlAgilityPack;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -15,14 +16,42 @@ using System.Windows.Shapes;
 
 namespace JNovelClubDL
 {
+    [PropertyChanged.AddINotifyPropertyChangedInterface]
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
     public partial class MainWindow : Window
     {
+        HtmlDocument doc = new HtmlDocument();
+
+        public string Status { get; set; }
+        public bool DownloadEnabled { get; set; }
+
         public MainWindow()
         {
             InitializeComponent();
+            Status = "Loading Chromium engine...";
+        }
+
+        private async void browser_SourceUpdated(object sender, DataTransferEventArgs e)
+        {
+            var html = await browser.GetBrowser().MainFrame.GetSourceAsync();
+            doc.LoadHtml(html);
+            if(doc.DocumentNode.Descendants("div").Where(o => o.GetAttributeValue("class", "").Contains("PartReader__content")).FirstOrDefault() != null)
+            {
+                Status = "Click Download button to save the currently open chapter.";
+                DownloadEnabled = true;
+            }
+            else
+            {
+                Status = "Open novel reader first.";
+                DownloadEnabled = false;
+            }
+        }
+
+        private void TryDownloadNovel(object sender, RoutedEventArgs e)
+        {
+            // TODO
         }
     }
 }
